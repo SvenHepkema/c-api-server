21b65c5ef689ec40ada44665eb51cc70dc0f11f7
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-02-09 14:37:06 +0100

    Added multithreading to the benchmarking script

--- benchmark-scripts/main.py
+++ benchmark-scripts/main.py
@@ -4,38 +4,65 @@ import datetime
 import requests
 import argparse
 import logging
+import threading
 
+DEFAULT_WAIT_MS = 200
 DEFAULT_PORT = 8080;
 URL = f"http://localhost:{DEFAULT_PORT}/";
 
-def empty_request(url: str, milliseconds: int) -> int:
+
+
+def empty_request_executor(index: int, start: datetime.datetime, duration_ms: int, url: str, results: list) -> None:
     requests_made = 0
-    start = datetime.datetime.now()
 
-    microseconds = milliseconds * 1000 # convert milliseconds to microseconds
+    while datetime.datetime.now() < start:
+        pass
 
-    while (datetime.datetime.now() - start).microseconds < microseconds:
+    while (datetime.datetime.now() - start).microseconds < duration_ms:
         try:
             requests.get(url)
         except:
             logging.error(f"Sending an empty request to {url} failed.")
             break
-
         requests_made += 1
 
-    logging.info(f"\t'{url}' within {milliseconds}ms there were \t{requests_made} requests")
+    results[index] = requests_made
+    
+
+
+def empty_request(url: str, milliseconds: int, n_threads: int) -> int:
+    microseconds = milliseconds * 1000 # convert milliseconds to microseconds
+
+    start = datetime.datetime.now() + datetime.timedelta(milliseconds=(DEFAULT_WAIT_MS))
+    requests_made = 0
+    threads = []
+    results = [0] * n_threads
+
+    for i in range(n_threads):
+        threads.append(threading.Thread(target=empty_request_executor, args=(i, start, microseconds, url, results)))
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+    
+    requests_made = sum(results)
+
+    logging.info(f"\t'{url}' within {milliseconds}ms {n_threads} threads made \t{requests_made} requests")
     
     return requests_made
 
 
 def main(args):
     logging.basicConfig(encoding='utf-8', level=logging.INFO)
-    empty_request(URL + args.path, args.ms)
+    empty_request(URL + args.path, args.ms, args.n_threads)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="path to fire requests at", type=str)
     parser.add_argument("--ms", help="duration in milliseconds", type=int, default=500)
+    parser.add_argument("--n_threads", help="number of threads", type=int, default=10)
     args = parser.parse_args()
     main(args)
